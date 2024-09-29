6832cbe6f74ca4d0caf205ce8f1352b7ce289bc6
zhch02 <132458165+zhch02@users.noreply.github.com>
2024-09-29 15:59:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '610a5d83-62ef-4e4c-91d8-1d9f9c3d0c72';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
